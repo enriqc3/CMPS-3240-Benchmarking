06489bf1d1a61b4fc6521c83668e0a90dbf99504
Albert C Cruz <acruz37@csub.edu>
2018-11-28 15:46:24 -0800

    Daily

--- myblas.c
+++ myblas.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include "myblas.h"
 
+#define DAXPY(i, a, X, Y) (a) * (X)[i] + (Y)[i]
+
+// RAND_T for random using a template. 
+// C does not support templates, so use macros instead
+#define RAND_T(type) (type) rand()
+
 /********************************************
  * AXPY operations
  * A linear algebra operation that carries out
@@ -16,19 +22,19 @@
 // Integer version
 void iaxpy( const int length, int a, int *X, int *Y, int *Result ) {
 	for( int i = 0; i < length; i++ )
-		Result[i] = a * X[i] + Y[i];
+		Result[i] = DAXPY(i, a, X, Y);
 }
 
 // Float version
 void faxpy( const int length, float a, float *X, float *Y, float *Result ) {
 	for( int i = 0; i < length; i++ )
-		Result[i] = a * X[i] + Y[i];
+		Result[i] = DAXPY(i, a, X, Y);
 }
 
 // Double version
 void daxpy( const int length, double a, double *X, double *Y, double *Result ) {
 	for( int i = 0; i < length; i++ )
-		Result[i] = a * X[i] + Y[i];
+		Result[i] = DAXPY(i, a, X, Y);
 }
 
 /********************************************
@@ -38,38 +44,38 @@ void daxpy( const int length, double a, double *X, double *Y, double *Result ) {
 // Code to randomly initialize a vector
 void d_initVect( const int n, double* A ) {
   for ( int i = 0; i < n; i++ ) 
-    A[ i ] = 2 * rand() / (double) RAND_MAX - 1; // [-1,1]
+    A[ i ] = RAND_T(double); 
 }
 
 // Code to randomly initialize a matrix
 void d_initMat( const int n, double* A ) {
    for ( int i = 0; i < n; i++ ) 
       for ( int j = 0; j < n; j++ ) 
-         A[ i + j * n ] = 2 * rand() / (double) RAND_MAX - 1; // [-1,1]
+         A[ i + j * n ] = RAND_T(double);
 }
 
 // Code to randomly initialize a vector
 void f_initVect( const int n, float* A ) {
   for ( int i = 0; i < n; i++ ) 
-    A[ i ] = 2 * rand() / (float) RAND_MAX - 1; // [-1,1]
+    A[ i ] = RAND_T(float); // [-1,1]
 }
 
 // Code to randomly initialize a matrix
 void f_initMat( const int n, float* A ) {
    for ( int i = 0; i < n; i++ ) 
       for ( int j = 0; j < n; j++ ) 
-         A[ i + j * n ] = 2 * rand() / (float) RAND_MAX - 1; // [-1,1]
+         A[ i + j * n ] = RAND_T(float); // [-1,1]
 }
 
 // Code to randomly initialize a vector
 void i_initVect( const int n, int* A ) {
   for ( int i = 0; i < n; i++ ) 
-    A[ i ] = 2 * rand() / (double) RAND_MAX - 1; // [-1,1]
+    A[ i ] = rand(); // [-1,1]
 }
 
 // Code to randomly initialize a matrix
 void i_initMat( const int n, int* A ) {
    for ( int i = 0; i < n; i++ ) 
       for ( int j = 0; j < n; j++ ) 
-         A[ i + j * n ] = 2 * rand() / (double) RAND_MAX - 1; // [-1,1]
+         A[ i + j * n ] = rand(); // [-1,1]
 }
--- test_program.c
+++ test_program.c
@@ -5,6 +5,25 @@
 
 #include "myblas.h"
 
+// For convenience, changes types and function names with macros.
+#define DP
+
+#ifdef DP 
+  #define TYPE double
+  #define CONST_VAL 1.1
+  #define OPERATION daxpy
+#endif
+#ifdef SP 
+  #define TYPE float
+  #define CONST_VAL 1.1
+  #define OPERATION faxpy
+#endif
+#ifdef INT 
+  #define TYPE int
+  #define CONST_VAL 1
+  #define OPERATION iaxpy
+#endif
+
 int	main( int argc, char *argv[] ) {
    if( argc != 2 ) {
      printf( "Usage: ./test_program.out N ... where N is the length of one size of the vector\n"  );
@@ -18,23 +37,16 @@ int	main( int argc, char *argv[] ) {
    const int N = atoi( argv[1] );    
    printf( "Running operation of size %d x 1", N );
 
-   /* Create three N x 1 double precision floating point matrixes on the heap
-    * using malloc, also create a scalar double A.
+   /* Create three N x 1 matrixes on the heap
+    * using malloc, also create a scalar
     */
-   double A = 0.1; // Some arbitrary value for scalar A
-   double *X = (double *) malloc( N * sizeof(double) );   // First vector
-   double *Y = (double *) malloc( N * sizeof(double) );   // Second vector
-   double *Result = (double *) malloc( N * sizeof(double) );   // Result vector
-  
-   /* The following code loads random values into the matrixes.
-    * We don't need to initialize 'C' because it will hold the
-    * result.
-    */
-   d_initVect( N, X );  // Initialize values in A
-   d_initVect( N, Y );  // ... safe for B
-
-   // Carry out DAXPY
-   daxpy( N, A, X, Y, Result );
+   TYPE A = CONST_VAL; // Some arbitrary value for scalar A
+   TYPE *X = (TYPE *) malloc( N * sizeof(TYPE) );   // First vector
+   TYPE *Y = (TYPE *) malloc( N * sizeof(TYPE) );   // Second vector
+   TYPE *Result = (TYPE *) malloc( N * sizeof(TYPE) );   // Result vector
+   
+   // Carry out the operation
+   OPERATION( N, A, X, Y, Result );
 
    // Free up the memory
    free( X );
