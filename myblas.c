a5cf7c1f5126523eef0b64dcb295ea3780dbb152
Albert Cruz <acruz37@csub.edu>
2019-01-02 22:36:58 -0800

    Update myblas.c

--- myblas.c
+++ myblas.c
@@ -11,8 +11,8 @@
 
 // Integer version
 void iaxpy( int length, int a, int *X, int *Y, int *Result ) {
-	for( int i = 0; i < length; i++ )
-		Result[i] = a * X[i] + Y[i];
+    for( int i = 0; i < length; i++ )
+        Result[i] = a * X[i] + Y[i];
 }
 
 /********************************************
@@ -41,12 +41,9 @@ void dgemm ( int n, double *X, double *Y, double *Result) {
  * of the same length.
  ********************************************/
 
-void fdot (int n, float* A, float* B, double* C) {
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j) {
-            double cij = C[i+j*n]; /* cij = C[i][j] */
-            for( int k = 0; k < n; k++ )
-                cij += A[i+k*n] * B[k+j*n]; /* cij += A[i][k]*B[k][j] */
-            C[i+j*n] = cij; /* C[i][j] = cij */
-        }
+single fdot( int length, single *X, single *Y ) {
+    single Result = 0;
+    for( int i = 0; i < length; i++ )
+        Result += X[i] * Y[i];
+    return Result;
 }
